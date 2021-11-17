1dd47b200bdd457145316cec802f5e48af787dbf
kasian777 <kasian.dima@ukr.net>
2021-11-17 18:49:30 +0200

    3f

---
+++ lesson3/homework/main.js
@@ -0,0 +1,132 @@
+////////////////////////////////
+let oll = [5, 6, 14, 1, 8,
+    'dima', 'kasian', 'ivanov', 'oper', 'vivo',
+    4, 7, 'true', 'false', 'civik',
+    'nambr', 'strn', 'vimigr']
+for (const ol of oll) {
+    console.log(ol)
+}
+
+// /////////////////////////
+let name = [
+    'dima', 'kasian', 'd']
+
+// for (const jo of name) {
+console.log(name[0])
+// }
+
+//////////////////////////////////////////
+
+let i = 1
+while (i < 21) {
+
+    document.write(`<div>${i}</div>`)
+    i++
+}
+///////////////////////////////////////////
+let i2 = 1
+while (i2 < 21) {
+    document.write(`<h1> jbiubbo ${i2}</h1>`)
+
+    i2++
+}
+//////////////////////////////////////////
+let ff = [1, 2, 3, 5, 8, 9, 10, 15, 88, 99]
+for (const hh of ff) {
+    console.log(hh)
+}
+///////////////////////////////////////////
+let ctr = ['pushkin', 'dostoevskiy', 'shevchenko', 'ivan',
+    'denia', 'gaga', 'maga', 'baga', 'faga', 'dada']
+// let calc = 0
+for (const kol of ctr) {
+    // console.log(kol, calc++)
+    console.log(kol,)
+}
+//////////////////////////////////////////
+let faf = ['dima', 'kasian', 'ivanov', 'oper', 'vivo',
+    4, 7, 'true', 'false', 'civik',
+    'nambr', 'strn', 'vimigr']
+for (const hol of faf) {
+    console.log(hol);
+}
+//////////////////////////////////////////
+let namberr = [
+    true, false, 1, 2, 3, 4, 5, 'dad', 'mam', 'sister'
+]
+for (let fs = 0; fs < namberr.length; fs++) {
+    if (typeof namberr[fs] === 'number') {
+        console.log(namberr[fs])
+    }
+}
+//////////////////////////////////////////
+let bulya = [
+    true, false, 1, 2, 3, 4, 5, 'dad', 'mam', 'sister'
+]
+for (let fs = 0; fs < bulya.length; fs++) {
+    if (typeof bulya[fs] === 'boolean') {
+        console.log(bulya[fs])
+    }
+}
+///////////////////////////////////////////
+let srtring = [
+    true, false, 1, 2, 3, 4, 5, 'dad', 'mam', 'sister'
+]
+for (let fs = 0; fs < srtring.length; fs++) {
+    if (typeof srtring[fs] === 'string') {
+        console.log(srtring[fs])
+    }
+}
+/////////////////////////////////////////////
+let pusto = []
+pusto[0] = 2424;
+pusto[1] = 21321;
+pusto[2] = 'folse';
+pusto[3] = 'true'
+pusto[4] = 'scb';
+pusto[5] = 'nzbcv';
+pusto[6] = 'bsf';
+pusto[7] = 'vzc'
+pusto[8] = 'scb';
+pusto[9] = 'nzbcv';
+
+for (let gasa of pusto) {
+    console.log(gasa)
+}
+////////////////////////////////////////////////
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    document.write(i)
+}
+////////////////////////////////////////////
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    document.write(i)
+}
+////////////////////////////////////////////
+for (let i = 0; i < 100; i=i+2)
+if (i%2 ===0 && i!==0)
+{
+    console.log(i);
+    document.write('<div>')
+    document.write(i)
+    document.write('</div>')
+}
+////////////////////////////////////////
+for (let i = 0; i < 100; i=i+2)
+
+    {
+        console.log(i);
+        document.write('<div>')
+        document.write(i)
+        document.write('</div>')
+    }
+/////////////////////////////////////////
+for (let i = 0; i < 100; i++)
+    if (i%2 !==0)
+    {
+        console.log(i);
+        document.write('<div>')
+        document.write(i)
+        document.write('</div>')
+    }
