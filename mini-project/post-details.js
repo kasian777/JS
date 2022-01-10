5b939fc7254cb8a4806338d1d528ef2570e4cfe1
kasian777 <kasian.dima@ukr.net>
2022-01-11 00:03:25 +0200

    full

--- mini-project/post-details.js
+++ mini-project/post-details.js
@@ -2,23 +2,27 @@ let mas = localStorage.getItem('key')
 let key = JSON.parse(mas)
 let keynum = Number(key)
 
-fetch(`https://jsonplaceholder.typicode.com/posts`)
+fetch(`https://jsonplaceholder.typicode.com/comments`)
     .then(response => response.json())
     .then(fullinfopost => {
         for (info of fullinfopost){
-            if(keynum === info.userId ){
+            if(keynum === info.postId ){
 
                 let div = document.createElement('div');
                 let h3 = document.createElement('h3');
                 let p = document.createElement('p');
                 let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
                 div.classList.add('blockforpost')
 
-                h3.innerText = `${info.userId}`;
-                p.innerText = `${info.title}`;
-                p.innerText = `${info.body}`;
+                h3.innerText = `${info.postId}`;
+                p.innerText = `id : ${info.id}`;
+                p2.innerText = ` name : ${info.name}`;
+                p3.innerText = `${info.email}`;
+                p4.innerText = ` body : ${info.body}`;
 
-                div.append(h3, p, p2 )
+                div.append(h3, p, p2, p3, p4 )
                 document.body.append(div);
             }
         }
